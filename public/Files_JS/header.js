dddcda851cfc59241d31299cd1a83e062d6219f6
ManuTissera <manuel.tisseras@gmail.com>
2024-11-28 15:05:39 +0100

    update portfolio

--- public/Files_JS/header.js
+++ public/Files_JS/header.js
@@ -1,8 +1,25 @@
 
 
-const name_profile = document.querySelector('.name-profile');
-const category_profile = document.querySelector('.category-profile');
+const name_profile_aside = document.querySelector('.name-profile-aside');
+const name_profile_header = document.querySelector('.name-profile-header');
+const category_profile_aside = document.querySelector('.category-profile-aside');
+const category_profile_header = document.querySelector('.category-profile-header');
 
+name_profile_aside.textContent = 'Tissera Manuel'
+category_profile_aside.textContent = 'Administrador'
+name_profile_header.textContent = 'Tissera Manuel'
+category_profile_header.textContent = 'Administrador'
 
+const URLQueryHeader = window.location.origin;
 
-console.log('Header funciona')
+localStorage.setItem('profileName','Manuel')
+localStorage.setItem('profileCategory','Director');
+
+const getDataPersonal = async () =>{
+
+   let personalResult = await fetch(`${URLQueryHeader}/view_personal`).then(res=>res.json()).then(data=>data)
+
+   console.log(personalResult);
+}
+
+getDataPersonal();
