39be7ecdca2f9bb3a4057ac356fbb5b50a4dae97
ManuTissera <manuel.tisseras@gmail.com>
2025-12-10 00:53:58 +0100

    soportar

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 console.clear();
 
 import express from "express";
-import routerRequest from "./Routes/routes.js";
-import requestRouterData from "./Routes/routes-news-data.js"
-import connection from "./Routes/connectionBBDD.js";
+import connection from "./Routers/connectionBBDD.js";
+import routerApp from "./Routers/routes.js";
 import cors from "cors";
-import path from 'path';
 
 const expressApp = express();
-const PORT = process.env.PORT || 3210;
 
-const __dirname = path.resolve();
-expressApp.use(express.static(path.join(__dirname, 'public')));
+// Heroku pone el puerto en process.env.PORT
+const PORT = process.env.PORT || 3111;
 
 expressApp.use(express.json());
 expressApp.use(express.text());
 expressApp.use(cors());
+expressApp.use(routerApp);
 
-expressApp.use("/",requestRouterData);
-expressApp.use("/",routerRequest);
-
-expressApp.listen(PORT,()=>{
-   console.log('Listen in PORT= '+ PORT)
-   console.log('Prueba con path en routes.js')
-})
-
-
-
+expressApp.listen(PORT, () => {
+  console.log("Server listen port: " + PORT);
+});
