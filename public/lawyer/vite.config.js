bb36901bc7eb0aa6c1a354fb652f5f307bd7a377
ManuTissera <manuel.tisseras@gmail.com>
2026-08-14 11:51:50 -0300

    nuevo9

--- public/lawyer/vite.config.js
+++ public/lawyer/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/lawyer/',
   plugins: [react()],
+  // ...el resto de tu config existente
 })
